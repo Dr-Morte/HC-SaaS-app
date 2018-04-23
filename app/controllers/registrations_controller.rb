3ede8d79e1a91d738afa71dc21e1dd509fe475f8
EC2 Default User <ec2-user@ip-172-31-13-14.us-east-2.compute.internal>
2018-04-23 03:40:13 +0000

    if/else for admin, add stars on signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def sign_up_params
     params.require(:user).permit(:username, :phone, :email, :password, :password_confirmation)
-    current_user.increment(:stars, 2)
+    User.increment(:stars, 2)
   end
 
   def account_update_params
