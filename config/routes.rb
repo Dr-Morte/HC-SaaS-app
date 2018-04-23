32f7964eba13e0075072ab0823e7ac51b3d3d591
EC2 Default User <ec2-user@ip-172-31-13-14.us-east-2.compute.internal>
2018-04-23 01:56:52 +0000

    ugh heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :widgets
   devise_for :users, :controllers => { registrations: 'registrations' }
-  root 'startup/index'
+  root 'startup#index'
   get 'startup/contacts', to: 'startup#contacts'
   get 'startup/index', to: 'startup#index'
   get 'startup/events', to: 'startup#events'
